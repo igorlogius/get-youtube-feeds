0e7211b189f995c2f09bf2fc3ca347966a78d822
igorlogius <67047467+igorlogius@users.noreply.github.com>
2020-06-20 12:13:51 +0200

    cleanup

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-build-web-ext/build.sh
+#!/bin/bash
+type web-ext >/dev/null
+RET=$?
+WEBEXT_CMD='web-ext build -s ./src/ -a . --overwrite-dest'
+if [ $RET -ne 0 ];
+then
+	npm install web-ext --prefix $(pwd)
+	eval ./node_modules/.bin/$WEBEXT_CMD
+else
+	eval "$WEBEXT_CMD"
+fi
